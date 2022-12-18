f6fa929a89f8dfc9fc483025f42b869bd47085cf
lijo J Palathingal <lijojpalathingal@gmail.com>
2022-12-18 11:32:23 +0530

    understand common js module

--- app.js
+++ app.js
@@ -1,10 +1,29 @@
-const http = require("http")
 
-const server =http.createServer((req,res)=>{
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify({
-        data: 'Hello World!'
-      }));
-});
+const Circles = require('./circle')
+const circles =new Circles
+console.log(circles.area(5))
+console.log(circles.circumferance(5))
 
-server.listen(3000,()=>{console.log('server running')})
+
+
+// ==================================object destructring======
+
+// const {area,circumferance}= require('./circle')
+
+// console.log(area(5))
+// console.log(circumferance(5))
+
+// ==================================object destructring=======
+
+
+
+// const http = require("http")
+
+// const server =http.createServer((req,res)=>{
+//     res.writeHead(200, { 'Content-Type': 'application/json' });
+//     res.end(JSON.stringify({
+//         data: 'Hello World!'
+//       }));
+// });
+
+// server.listen(3000,()=>{console.log('server running')})
---
+++ circle.js
@@ -0,0 +1,22 @@
+class Circles{
+    area(r) {
+        return Math.PI*r*r
+    }
+    circumferance (r){
+        return Math.PI*2*r
+    }
+}
+
+module.exports = Circles;
+
+
+
+
+
+
+// exports.area = function (r) {
+//     return Math.PI*r*r
+// }
+// exports.circumferance = function (r) {
+//     return Math.PI*2*r
+// }
