48389324685cf8feaf735914bc0f2f9ef15e38fe
lijo J Palathingal <lijojpalathingal@gmail.com>
2022-12-18 15:57:06 +0530

    use nodemon

--- app.js
+++ app.js
@@ -1,29 +1,10 @@
+const http = require("http")
+const PORT = 4000;
 
-const Circles = require('./circle')
-const circles =new Circles
-console.log(circles.area(5))
-console.log(circles.circumferance(5))
-
-
-
-// ==================================object destructring======
-
-// const {area,circumferance}= require('./circle')
-
-// console.log(area(5))
-// console.log(circumferance(5))
-
-// ==================================object destructring=======
-
-
-
-// const http = require("http")
-
-// const server =http.createServer((req,res)=>{
-//     res.writeHead(200, { 'Content-Type': 'application/json' });
-//     res.end(JSON.stringify({
-//         data: 'Hello World!'
-//       }));
-// });
-
-// server.listen(3000,()=>{console.log('server running')})
+const server =http.createServer((req,res)=>{
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({
+        data: 'Hello World!'
+      }));
+});
+server.listen(PORT,()=>{console.log('server running at http://localhost:',PORT)})
