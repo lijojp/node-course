86d04321dc2381988ccd2ecf3d16fc274b91cf7d
lijo J Palathingal <lijojpalathingal@gmail.com>
2022-12-31 20:26:58 +0530

    JWT authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,5 +1,25 @@
-const auth = function(req,res,next){
-    res.status(401).json({error:"unauthorized user"})
- 
+var jwt = require('jsonwebtoken');
+const { ObjectId } = require('mongodb');
+const User = require('../models/user');
+
+const auth =async function(req,res,next){
+    if(req.headers &&
+         req.headers.authorization &&
+         req.headers.authorization.split(" ")[1]
+         ){
+        const token = (req.headers.authorization.split(" ")[1]);
+        try {
+            const decode = await jwt.verify(token,"fake-jwt-secret")
+            const user = await User.findOne({ _id: ObjectId(decode.user._id)})
+            if(!user){
+                res.status(401).json({error:"unauthorized user"})
+                return;
+            }
+            next()
+        } catch (error) {
+            res.status(401).json({ error : "unauthorized user" })
+            console.log(error)
+        }
+    }
 }
 module.exports = auth;
