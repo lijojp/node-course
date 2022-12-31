399fdaffec6830fd4b1ba2914acd9d62edeb2c33
lijo J Palathingal <lijojpalathingal@gmail.com>
2022-12-31 11:25:35 +0530

    JWT access token

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,7 @@ const authRouter = express.Router()
 const AuthController =require("../controllers/AuthController")
 
 
-authRouter
-
-.post("/signup",AuthController.signup)
+authRouter.post("/signup",AuthController.signup)
+authRouter.post ("/login",AuthController.login)
 
 module.exports = authRouter
